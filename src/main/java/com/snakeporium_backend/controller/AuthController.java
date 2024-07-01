a466fef2da2db2f7a0178a7d76b072813a617208
sms165 <61697688+sms165@users.noreply.github.com>
2024-07-01 19:36:26 +0200

    ai add detail

--- src/main/java/com/snakeporium_backend/controller/AuthController.java
+++ src/main/java/com/snakeporium_backend/controller/AuthController.java
@@ -42,6 +42,7 @@ import java.util.Optional;
 @CrossOrigin(origins = "https://snakeporium.yblackbox.com")
 public class AuthController {
 
+
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
     private final UserRepository userRepository;
