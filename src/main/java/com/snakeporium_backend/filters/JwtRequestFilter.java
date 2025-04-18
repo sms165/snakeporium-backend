b1b44e484cb98b639f380dcd44bc850f6f2ad8b7
sms165 <61697688+sms165@users.noreply.github.com>
2025-04-18 19:21:57 +0200

    Update JwtRequestFilter.java

--- src/main/java/com/snakeporium_backend/filters/JwtRequestFilter.java
+++ src/main/java/com/snakeporium_backend/filters/JwtRequestFilter.java
@@ -1,15 +1,9 @@
 package com.snakeporium_backend.filters;
 
 
-import com.snakeporium_backend.services.jwt.UserDetailsServiceImpl;
-import com.snakeporium_backend.utils.JwtUtil;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,8 +11,16 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.logging.Logger;
+import com.snakeporium_backend.services.jwt.UserDetailsServiceImpl;
+import com.snakeporium_backend.utils.JwtUtil;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
@@ -49,6 +51,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             }
         } else {
             System.out.println("JWT Token does not begin with Bearer String");
+            System.out.println(authHeader);
         }
 
 
